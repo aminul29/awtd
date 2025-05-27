6fe796d21340e05b359b9e30523a63d3605cb780
aminul29 <89416308+aminul29@users.noreply.github.com>
2025-05-27 20:55:29 +0600

    registered tailwind output css in functions

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * awtd functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package awtd
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+if (! defined('_S_VERSION')) {
+    // Replace the version number of the theme on each release.
+    define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,88 +20,89 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function awtd_setup() {
-	/*
+function awtd_setup()
+{
+    /*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on awtd, use a find and replace
 		* to change 'awtd' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'awtd', get_template_directory() . '/languages' );
+    load_theme_textdomain('awtd', get_template_directory() . '/languages');
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support('automatic-feed-links');
 
-	/*
+    /*
 		* Let WordPress manage the document title.
 		* By adding theme support, we declare that this theme does not use a
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
-	/*
+    /*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'awtd' ),
-		)
-	);
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus(
+        array(
+            'menu-1' => esc_html__('Primary', 'awtd'),
+        )
+    );
 
-	/*
+    /*
 		* Switch default core markup for search form, comment form, and comments
 		* to output valid HTML5.
 		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
+    add_theme_support(
+        'html5',
+        array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script',
+        )
+    );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'awtd_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
+    // Set up the WordPress core custom background feature.
+    add_theme_support(
+        'custom-background',
+        apply_filters(
+            'awtd_custom_background_args',
+            array(
+                'default-color' => 'ffffff',
+                'default-image' => '',
+            )
+        )
+    );
 
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+    // Add theme support for selective refresh for widgets.
+    add_theme_support('customize-selective-refresh-widgets');
 
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+    /**
+     * Add support for core custom logo.
+     *
+     * @link https://codex.wordpress.org/Theme_Logo
+     */
+    add_theme_support(
+        'custom-logo',
+        array(
+            'height'      => 250,
+            'width'       => 250,
+            'flex-width'  => true,
+            'flex-height' => true,
+        )
+    );
 }
-add_action( 'after_setup_theme', 'awtd_setup' );
+add_action('after_setup_theme', 'awtd_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,45 +111,50 @@ add_action( 'after_setup_theme', 'awtd_setup' );
  *
  * @global int $content_width
  */
-function awtd_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'awtd_content_width', 640 );
+function awtd_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('awtd_content_width', 640);
 }
-add_action( 'after_setup_theme', 'awtd_content_width', 0 );
+add_action('after_setup_theme', 'awtd_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function awtd_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'awtd' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'awtd' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+function awtd_widgets_init()
+{
+    register_sidebar(
+        array(
+            'name'          => esc_html__('Sidebar', 'awtd'),
+            'id'            => 'sidebar-1',
+            'description'   => esc_html__('Add widgets here.', 'awtd'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
 }
-add_action( 'widgets_init', 'awtd_widgets_init' );
+add_action('widgets_init', 'awtd_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function awtd_scripts() {
-	wp_enqueue_style( 'awtd-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'awtd-style', 'rtl', 'replace' );
+function awtd_scripts()
+{
+    wp_enqueue_style('awtd-style', get_stylesheet_uri(), array(), _S_VERSION);
+    // enque tailwind css
+    wp_enqueue_style('awtd-tailwind', get_template_directory_uri() . './assets/css/tailwind-output.css', array(), _S_VERSION);
+    wp_style_add_data('awtd-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'awtd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script('awtd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'awtd_scripts' );
+add_action('wp_enqueue_scripts', 'awtd_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +179,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
-

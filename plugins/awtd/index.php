8aa67051c7501a93793b7f074a11cdf5350099fa
aminul29 <89416308+aminul29@users.noreply.github.com>
2025-06-13 10:58:45 +0600

    Create and activate a custom plugin awtd to extend the functionalities of the theme

---
+++ plugins/awtd/index.php
@@ -0,0 +1,9 @@
+<?php  
+/*
+Plugin Name: EXtend Advance WordPress Theme
+Plugin URI: http://mhdaminul.com
+Description: Advance WordPress theme
+Version: 1.0.0
+Author: Md Aminul
+Author URI: http://mhdaminul.com
+*/
